a6f8f337bd98939f2bfbb3edeb27c4fd6fd19d5c
spoonybard <rukaruth@gmail.com>
2018-01-23 18:15:16 -0600

    now the loaded data is accessible outside the stupid function; just say model["wordsByLocation"] to get access to wordsByLocation, etc.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //TODO: add drag/drop capability for file upload
 
-//Function to read data from uploaded json file. Called on button click.
+let model;
+//Function to read data from uploaded json file. Called on button click. Saves data to localstorage as a string.
 function loadFile() {
     hideUploadScreen();
     let input, reader, data;
-
     reader = new FileReader();
     input = document.getElementById("json-file");
     if (!input.files[0]) {
@@ -13,33 +13,29 @@ function loadFile() {
     else {
         reader.onload = (function() {
             data = reader.result;
-            data = JSON.parse(data);
-
-            //load all the data directly into the metadata tab or as universally accessible json objects
-            document.getElementById("dataset").innerHTML = data["dataset"];
-            document.getElementById("topics").innerHTML = data["topics"];
-            document.getElementById("iterations").innerHTML = data["iterations"];
-            document.getElementById("alpha").innerHTML = data["alpha"];
-            document.getElementById("beta").innerHTML = data["beta"];
-            const wordsByLocation = data["wordsByLocation"];
-            const topicsByLocation = data["topicsByLocation"];
-            const wordCounts = data["wordCounts"];
-            const wordTopicCounts = data["wordTopicCounts"];
-            const topicList = data["topicList"];
-            const topicWordCounts = data["topicWordCounts"];
-            const docList = data["docList"];
-            const docWordCounts = data["docWordCounts"]
-        });
+            try {
+                localStorage.setItem("model", data);
+            }
+            catch (e) {
+                console.log("Storage failed: " + e);
+            }});
         reader.readAsText(input.files[0]);
     }
+    model = JSON.parse(localStorage.getItem("model"));
+    //load all the data directly into the metadata tab or as universally accessible json objects
+    document.getElementById("dataset").innerHTML = model["dataset"];
+    document.getElementById("topics").innerHTML = model["topics"];
+    document.getElementById("iterations").innerHTML = model["iterations"];
+    document.getElementById("alpha").innerHTML = model["alpha"];
+    document.getElementById("beta").innerHTML = model["beta"];
 }
 
+//Progress from welcome screen to data visualization tabs
 function hideUploadScreen() {
     let uploadbox= document.getElementById("file-upload");
     let tabs = document.getElementById("tabs");
     uploadbox.style.display = "none";
     tabs.style.display = "block";
-
 }
 
 //Tab switching function
@@ -47,3 +43,4 @@ $( function() {
     $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
     $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
 } );
+
