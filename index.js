150fb4e4904010ee88d2f9afdeca292b4d42a31d
spoonybard <rukaruth@gmail.com>
2018-02-12 23:12:16 -0600

    config file creation form

--- index.js
+++ index.js
@@ -1,9 +1,142 @@
 //TODO: add drag/drop capability for file upload
 //This variable is global because it will contain all our data
+let fs = require("fs");
 let model = {};
 
+function loadConfigForm(){
+    document.getElementById("welcome-page").style.display = "none";
+    document.getElementById("create-file-form").style.display = "block";
+}
+
+function loadFileChoice() {
+    document.getElementById("welcome-page").style.display = "none";
+    document.getElementById("file-upload").style.display = "block";
+}
+
+function createConfigFile(){
+    let source, iterations, topics, outputname, upperlimit, lowerlimit,
+        whitelist, blacklist, numberofdocuments, lengthofdocuments,
+        splitstring, alpha, beta;
+
+    let englishStopwords = ["a", "a's", "able", "about", "above", "according", "accordingly", "across", "actually", "after", "afterwards",
+        "again", "against", "ain't", "all", "allow", "allows", "almost", "alone", "along", "already", "also",
+        "although", "always", "am", "among", "amongst", "an", "and", "another", "any", "anybody", "anyhow", "anyone",
+        "anything", "anyway", "anyways", "anywhere", "apart", "appear", "appreciate", "appropriate", "are", "aren't",
+        "around", "as", "aside", "ask", "asking", "associated", "at", "available", "away", "awfully", "b", "be",
+        "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being",
+        "believe", "below", "beside", "besides", "best", "better", "between", "beyond", "both", "brief", "but", "by",
+        "c", "c'mon", "c's", "came", "can", "can't", "cannot", "cant", "cause", "causes", "certain", "certainly",
+        "changes", "clearly", "co", "com", "come", "comes", "concerning", "consequently", "consider", "considering",
+        "contain", "containing", "contains", "corresponding", "could", "couldn't", "course", "currently", "d",
+        "definitely", "described", "despite", "did", "didn't", "different", "do", "does", "doesn't", "doing", "don't",
+        "done", "down", "downwards", "during", "e", "each", "edu", "eg", "eight", "either", "else", "elsewhere",
+        "enough", "entirely", "especially", "et", "etc", "even", "ever", "every", "everybody", "everyone",
+        "everything", "everywhere", "ex", "exactly", "example", "except", "f", "far", "few", "fifth", "first", "five",
+        "followed", "following", "follows", "for", "former", "formerly", "forth", "four", "from", "further",
+        "furthermore", "g", "get", "gets", "getting", "given", "gives", "go", "goes", "going", "gone", "got",
+        "gotten", "greetings", "h", "had", "hadn't", "happens", "hardly", "has", "hasn't", "have", "haven't",
+        "having", "he", "he's", "hello", "help", "hence", "her", "here", "here's", "hereafter", "hereby", "herein",
+        "hereupon", "hers", "herself", "hi", "him", "himself", "his", "hither", "hopefully", "how", "howbeit",
+        "however", "i", "i'd", "i'll", "i'm", "i've", "ie", "if", "ignored", "immediate", "in", "inasmuch", "inc",
+        "indeed", "indicate", "indicated", "indicates", "inner", "insofar", "instead", "into", "inward", "is",
+        "isn't", "it", "it'd", "it'll", "it's", "its", "itself", "j", "just", "k", "keep", "keeps", "kept", "know",
+        "known", "knows", "l", "last", "lately", "later", "latter", "latterly", "least", "less", "lest", "let",
+        "let's", "like", "liked", "likely", "little", "look", "looking", "looks", "ltd", "m", "mainly", "many", "may",
+        "maybe", "me", "mean", "meanwhile", "merely", "might", "more", "moreover", "most", "mostly", "much", "must",
+        "my", "myself", "n", "name", "namely", "nd", "near", "nearly", "necessary", "need", "needs", "neither",
+        "never", "nevertheless", "new", "next", "nine", "no", "nobody", "non", "none", "noone", "nor", "normally",
+        "not", "nothing", "novel", "now", "nowhere", "o", "obviously", "of", "off", "often", "oh", "ok", "okay",
+        "old", "on", "once", "one", "ones", "only", "onto", "or", "other", "others", "otherwise", "ought", "our",
+        "ours", "ourselves", "out", "outside", "over", "overall", "own", "p", "particular", "particularly", "per",
+        "perhaps", "placed", "please", "plus", "possible", "presumably", "probably", "provides", "q", "que", "quite",
+        "qv", "r", "rather", "rd", "re", "really", "reasonably", "regarding", "regardless", "regards", "relatively",
+        "respectively", "right", "s", "said", "same", "saw", "say", "saying", "says", "second", "secondly", "see",
+        "seeing", "seem", "seemed", "seeming", "seems", "seen", "self", "selves", "sensible", "sent", "serious",
+        "seriously", "seven", "several", "shall", "she", "should", "shouldn't", "since", "six", "so", "some",
+        "somebody", "somehow", "someone", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon",
+        "sorry", "specified", "specify", "specifying", "still", "sub", "such", "sup", "sure", "t", "t's", "take",
+        "taken", "tell", "tends", "th", "than", "thank", "thanks", "thanx", "that", "that's", "thats", "the", "their",
+        "theirs", "them", "themselves", "then", "thence", "there", "there's", "thereafter", "thereby", "therefore",
+        "therein", "theres", "thereupon", "these", "they", "they'd", "they'll", "they're", "they've", "think",
+        "third", "this", "thorough", "thoroughly", "those", "though", "three", "through", "throughout", "thru",
+        "thus", "to", "together", "too", "took", "toward", "towards", "tried", "tries", "truly", "try", "trying",
+        "twice", "two", "u", "un", "under", "unfortunately", "unless", "unlikely", "until", "unto", "up", "upon",
+        "us", "use", "used", "useful", "uses", "using", "usually", "uucp", "v", "value", "various", "very", "via",
+        "viz", "vs", "w", "want", "wants", "was", "wasn't", "way", "we", "we'd", "we'll", "we're", "we've", "welcome",
+        "well", "went", "were", "weren't", "what", "what's", "whatever", "when", "whence", "whenever", "where",
+        "where's", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which",
+        "while", "whilst", "whither", "who", "who's", "whoever", "whole", "whom", "whose", "why", "will", "willing",
+        "wish", "with", "within", "without", "won't", "wonder", "would", "wouldn't", "x", "y", "yes", "yet", "you",
+        "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves", "z", "zero"];
+
+    let latinStopwords = ["ab", "ac", "ad", "adhic", "aliqui", "aliquis", "an", "ante", "apud", "at", "atque", "aut", "autem", "cum",
+        "cur", "de", "deinde", "dum", "ego", "enim", "ergo", "es", "est", "et", "etiam", "etsi", "ex", "fio", "haud",
+        "hic", "iam", "idem", "igitur", "ille", "in", "infra", "inter", "interim", "ipse", "is", "ita", "magis", "modo",
+        "mox", "nam", "ne", "nec", "necque", "neque", "nisi", "non", "nos", "o", "ob", "per", "possum", "post", "pro",
+        "quae", "quam", "quare", "qui", "quia", "quicumque", "quidem", "quilibet", "quis", "quisnam", "quisquam",
+        "quisque", "quisquis", "quo", "quoniam", "sed", "si", "sic", "sive", "sub", "sui", "sum", "super", "suus",
+        "tam", "tamen", "trans", "tu", "tum", "ubi", "uel", "uero", "unus", "ut"];
+
+    source = document.getElementById("create-file-source").value;
+    iterations = document.getElementById("create-file-iterations").value;
+    topics = document.getElementById("create-file-topics").value;
+    outputname = document.getElementById("create-file-output").value;
+    upperlimit = parseInt(document.getElementById("create-file-upperlimit").value) / 100;
+    lowerlimit = parseInt(document.getElementById("create-file-lowerlimit").value) / 100;
+    whitelist = document.getElementById("create-file-whitelist").value.split();
+    blacklist = document.getElementById("create-file-blacklist").value.split();
+
+    if (document.getElementById("create-file-default-english-stopwords").value === "true"){
+        blacklist.push.apply(blacklist, englishStopwords);
+    }
+    if (document.getElementById("create-file-default-english-stopwords").value === "true"){
+        blacklist.push.apply(blacklist, latinStopwords);
+    }
+
+    if(document.getElementById("chunking-number").checked) {
+        numberofdocuments = document.getElementById("create-file-number-documents").value;
+        lengthofdocuments = "off";
+        splitstring = "off";
+    } else if (document.getElementById("chunking-length").checked) {
+        numberofdocuments = "off";
+        lengthofdocuments = document.getElementById("create-file-length-documents").value;
+        splitstring = "off";
+    } else if (document.getElementById("chunking-splitstring").checked) {
+        numberofdocuments = "off";
+        lengthofdocuments = "off";
+        splitstring = document.getElementById("create-file-split-string").value;
+    }
+    alpha = document.getElementById("create-file-alpha").value;
+    beta = document.getElementById("create-file-beta").value;
+
+    let config = {}, reqparam = {}, stoptions = {}, choptions = {}, hyperparameters = {};
+    reqparam["source"] = source;
+    reqparam["iterations"] = iterations;
+    reqparam["topics"] = topics;
+    reqparam["output name"] = outputname;
+    stoptions["upper limit"] = upperlimit;
+    stoptions["lower limit"] = lowerlimit;
+    stoptions["whitelist"] = whitelist;
+    stoptions["blacklist"] = blacklist;
+    choptions["number of documents"] = numberofdocuments;
+    choptions["length of documents"] = lengthofdocuments;
+    choptions["split string"] = splitstring;
+    hyperparameters["alpha"] = alpha;
+    hyperparameters["beta"] = beta;
+
+    config["required parameters"] = reqparam;
+    config["stopword options"] = stoptions;
+    config["chunking options"] = choptions;
+    config["hyperparameters"] = hyperparameters;
+
+    fs.writeFile("./config.json", JSON.stringify(config), (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
 var colors  = ["#66c2a5", "#fc8d62", "#8da0cb"];
-//Function to read data from uploaded json file. Called on button click. Saves data to localstorage as a string.
+//Function to read data from uploaded json file. Called on button click.
 function loadFile() {
     hideUploadScreen();
     let input, reader;
@@ -88,6 +221,7 @@ $(document).ready(function() {
         let topic = $("#word-cloud-topic-select option:selected").val();
         createWordCloud(topic)
     });
+
 });
 
 //ANNOTATED TEXT TAB
