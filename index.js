d17b64bbef2ac7b7a59fbdde2f01da2f91f52f95
spoonybard <rukaruth@gmail.com>
2018-02-20 00:29:35 -0600

    added main menu button so that users can go back if they click the wrong thing

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ let fs = require("fs"),
 const colors  = ["#66c2a5", "#fc8d62", "#8da0cb"],
     scaledColors = ["hsl(161, 30%, 90%)", "hsl(17, 30%, 90%)", "hsl(222, 30%, 90%)", "hsl(161, 63%, 38%)", "hsl(17, 86%, 49%)", "hsl(222, 57%, 47%)"];
 
+function reloadMainMenu(){
+    document.getElementById("welcome-page").style.display = "block";
+    document.getElementById("create-file-form").style.display = "none";
+    document.getElementById("file-upload").style.display = "none";
+
+}
 
 /**
  * Transitions from welcome page to config file creation page on button click
